3336fee80069e4b6fc34aae292be459a1f115dcc
Ilya Shirokolobov <941354+ilyashir@users.noreply.github.com>
2026-05-18 14:09:40 +0000

    Show migrated screenshots at native size

--- src/theme/MDXComponents/Img/index.tsx
+++ src/theme/MDXComponents/Img/index.tsx
@@ -5,25 +5,16 @@ import type {Props} from '@theme/MDXComponents/Img';
 import styles from './styles.module.css';
 
 function transformImgClassName(className?: string): string {
-  return clsx(className, styles.img);
-}
-
-function isGitBookAsset(src: unknown): boolean {
-  return typeof src === 'string' && src.startsWith('/gitbook/assets/');
+  return clsx(className, styles.img, 'trik-mdx-img');
 }
 
 export default function MDXImg(props: Props): ReactNode {
-  const nativeSizeProps = isGitBookAsset(props.src)
-    ? {style: {...props.style, maxWidth: '100%'}}
-    : {};
-
   return (
     // eslint-disable-next-line jsx-a11y/alt-text
     <img
       decoding="async"
       loading="lazy"
       {...props}
-      {...nativeSizeProps}
       className={transformImgClassName(props.className)}
     />
   );
